7ca2409b7ed6002a8d576d206c1fc88d6073fb7e
Asad Maraka <asadmaraka@Asads-MacBook-Air.local>
2022-02-25 19:39:29 +0200

    icon chages

--- src/components/CityCurrentWeather.js
+++ src/components/CityCurrentWeather.js
@@ -6,7 +6,6 @@ class CityCurrentWeather extends React.Component {
     const weather = this.props.weather;
     const temp = weather.Temperature.Metric.Value;
     const weatherText= weather.WeatherText;
-    const icon = weather.WeatherIcon < 10 ? '0' + weather.WeatherIcon : weather.WeatherIcon;
     const date = new Date(weather.EpochTime * 1000);
     const currentDate = date.getDate() + '/' + (date.getMonth() + 1);
     return(
